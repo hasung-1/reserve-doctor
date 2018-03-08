1350833d3f376950a4593cbd590f126cfcd83d17
SkyHigh113 <rlatjdgkr113@naver.com>
2018-03-08 17:30:09 +0900

    병원예약 - 병원 회원가입 초기버전 V 1

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 # Create your models here.
 class User(AbstractUser):
     USER_TYPE_CHOICES = (
@@ -9,20 +8,26 @@ class User(AbstractUser):
       (3, 'admin'),
     )
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,default=1)
-    
+
+        
 class Hospital_User(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
-    sido = models.CharField(max_length=20,)
-    gungu = models.CharField(max_length=20,blank = True)
-    dong = models.CharField(max_length=20,blank = True)
-    addr = models.CharField(max_length = 50,blank = True)
-    tel = models.CharField(max_length=50,blank = True)
+
+    def __str__(self):
+        return str(self.user)
+
 
 class Personal_User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True,blank = True)
+    sido = models.CharField(max_length=20,blank = True)
+    gungu = models.CharField(max_length=20,blank = True)
+    dong = models.CharField(max_length=20,blank = True)
     addr = models.CharField(max_length=100,blank = True)
     phone = models.CharField(max_length=50,blank = True)
 
+    def __str__(self):
+        return str(self.user)
 
 #class Personel_User(model.Model):
     #user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
