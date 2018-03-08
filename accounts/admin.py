130c5f5fcf438fd159f532de6974d765a8c3c374
DESKTOP-5KOD5J3\yoonh <yoonhanl@gmail.com>
2018-03-08 10:33:37 +0900

    User Admin 변경(User_type이 보이도록)

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User,UserAdmin)
+#admin.site.register(User,UserAdmin)
+
+
+
+class MyUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('user_type',)}),
+    )
+    list_display=('username','user_type',)
+
+admin.site.register(User, MyUserAdmin)
